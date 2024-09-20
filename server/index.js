820ddae89943e17dc38d1a8dcbe87d824e3bea06
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2024-09-19 18:30:35 -0700

    final commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
@@ -8,25 +9,33 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", async (req, res) => {
-	res.send("Hello from DALL-E");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 const startServer = () => {
-	try {
-		connectDB(process.env.MONGODB_URL);
-		app.listen(8080, () =>
-			console.log("server has started on port http://localhost:8080")
-		);
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () =>
+      console.log("server has started on port http://localhost:" + PORT)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
